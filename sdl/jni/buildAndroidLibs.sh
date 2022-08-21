52ce590203d0e830399456c316ab7f9fb71c5405
Borja Vazquez <vazquezborja@hotmail.es>
2022-08-21 01:15:00 -0300

    Fix order of arguments in script

--- sdl/jni/buildAndroidLibs.sh
+++ sdl/jni/buildAndroidLibs.sh
@@ -2,15 +2,15 @@
 GDE_ANDROID_PATH="$1"
 NDK_PATH="$2"
 
-if [ -z "$NDK_PATH" ]
+if [ -z "$GDE_ANDROID_PATH" ]
 then
-    echo "First enter the path to NDK"
+    echo "First enter the path to GDEAndroid, ex: ~/Documents/GDEAndroid"
     exit 0
 fi
 
-if [ -z "$GDE_ANDROID_PATH" ]
+if [ -z "$NDK_PATH" ]
 then
-    echo "Second enter the path to GDEAndroid, ex: ~/Documents/GDEAndroid"
+    echo "Second enter the path to NDK"
     exit 0
 fi
 
