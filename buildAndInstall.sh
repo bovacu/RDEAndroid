288776979fa6e1b1ee9d2f514ed5b2cf1ea3624c
Borja Vazquez <vazquezborja@hotmail.es>
2022-09-10 23:53:24 -0300

    Updated buildAndInstall script

--- buildAndInstall.sh
+++ buildAndInstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GDE_ANDROID_PATH="$1"
 NDK_PATH="$2"
-BUILD_TYPE="$3"
+APP_HEADERS="$3"
+APP_SOURCES="$4"
+BUILD_TYPE="$5"
 
 if [ -z "$GDE_ANDROID_PATH" ]
 then
@@ -15,11 +17,22 @@ then
     exit 0
 fi
 
-if [ -z "$BUILD_TYPE" ]
+if [ -z "$APP_HEADERS" ]
 then
-    echo "Third enter the path build type, debug or release"
+    echo "Third enter the path to your app headers folder, ex: ~/Documents/MyApp/include"
     exit 0
 fi
 
-./build.sh "$GDE_ANDROID_PATH" "$NDK_PATH" "$BUILD_TYPE"
+if [ -z "$APP_SOURCES" ]
+then
+    echo "Fourth enter the path to your app sources folder, ex: ~/Documents/MyApp/src"
+    exit 0
+fi
+
+if [ -z "$BUILD_TYPE" ]
+then
+    echo "Fifth enter the build type, debug or release"
+    exit 0
+fi
+./build.sh "$GDE_ANDROID_PATH" "$NDK_PATH" "$APP_HEADERS" "$APP_SOURCES" "$BUILD_TYPE"
 adb -d install apk/"$BUILD_TYPE"/app-"$BUILD_TYPE".apk
