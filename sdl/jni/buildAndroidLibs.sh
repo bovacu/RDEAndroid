e31365cdbbcbc7f4691e8543122e0d45f4e645b8
Borja Vazquez <vazquezborja@hotmail.es>
2022-10-04 12:27:36 -0300

    Adding Chipmunk and removing Box2D

--- sdl/jni/buildAndroidLibs.sh
+++ sdl/jni/buildAndroidLibs.sh
@@ -28,5 +28,5 @@ then
     NDK_BUILD_TYPE=0
 fi
 
-# This is REALLY IMPORTANT!! It builds the .so libs for SDL, SDL_Image, SDL_Mixer, Box2d (more if in future more libs are added), through their corresponding Android.mk
+# This is REALLY IMPORTANT!! It builds the .so libs for SDL, SDL_Image, SDL_Mixer, Chipmunk (more if in future more libs are added), through their corresponding Android.mk
 "$NDK_PATH"/20.1.5948944/ndk-build APP_PLATFORM=android-21 NDK_DEBUG="$NDK_BUILD_TYPE" NDK_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/object NDK_LIBS_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/lib
