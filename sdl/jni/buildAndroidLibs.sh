d78e29a8a6059c38243d5a22568dfccf9e996f10
bovacu <vazquezborja@hotmail.es>
2023-11-03 22:53:49 -0300

    At least now it is compiling and opening in the phone, but crashing due to missing shader files

--- sdl/jni/buildAndroidLibs.sh
+++ sdl/jni/buildAndroidLibs.sh
@@ -29,4 +29,4 @@ then
 fi
 
 # This is REALLY IMPORTANT!! It builds the .so libs for SDL, SDL_Image, SDL_Mixer, Chipmunk (more if in future more libs are added), through their corresponding Android.mk
-"$NDK_PATH"/20.1.5948944/ndk-build APP_PLATFORM=android-21 NDK_DEBUG="$NDK_BUILD_TYPE" NDK_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/object NDK_LIBS_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/lib
+"$NDK_PATH"/25.2.9519653/ndk-build APP_PLATFORM=android-21 NDK_DEBUG="$NDK_BUILD_TYPE" NDK_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/object NDK_LIBS_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/lib
