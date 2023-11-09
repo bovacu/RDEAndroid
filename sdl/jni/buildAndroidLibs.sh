815795bada24bbd54c8a16045369b7c401c1177c
bovacu <vazquezborja@hotmail.es>
2023-11-08 22:50:49 -0300

    Update parameters

--- sdl/jni/buildAndroidLibs.sh
+++ sdl/jni/buildAndroidLibs.sh
@@ -29,4 +29,4 @@ then
 fi
 
 # This is REALLY IMPORTANT!! It builds the .so libs for SDL through its corresponding Android.mk
-"$NDK_PATH"/25.2.9519653/ndk-build APP_PLATFORM=android-21 NDK_DEBUG=0 NDK_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/object NDK_LIBS_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/lib
+"$NDK_PATH"/25.2.9519653/ndk-build APP_PLATFORM=android-28 NDK_DEBUG=0 NDK_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/object NDK_LIBS_OUT="$GDE_ANDROID_PATH"/sdl/build/intermediates/ndkBuild/"$BUILD_TYPE"/lib
