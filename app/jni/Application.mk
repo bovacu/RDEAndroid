2685fb3bccdb448ef3e6a2f992611c332c5faf8b
Borja Vazquez <vazquezborja@hotmail.es>
2023-11-23 22:38:33 -0300

    ABIs x86_64 and x86 are working again. Made Jolt work on Android. Improved gradle.build on sdl and app

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_STL := c++_shared
-# x86_64 and x86 seem to be deprecated mostly (about 1% of the whole android phones)
-APP_ABI := armeabi-v7a arm64-v8a
-APP_PLATFORM := android-21
+APP_ABI := x86 x86_64 armeabi-v7a arm64-v8a
+APP_PLATFORM := android-25
+APP_OPTIM := release
